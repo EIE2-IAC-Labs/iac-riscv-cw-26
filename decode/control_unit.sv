ba1fe7ef39e0a77e99b548f96fc3715fb57fe0ff
Eliminate-Entrant <mshaheerch@hotmail.com>
2022-12-08 22:33:00 +0000

    Minor fixes

--- decode/control_unit.sv
+++ decode/control_unit.sv
@@ -9,7 +9,7 @@ module control_unit #(
     output logic [1:0] ResultSrcD,
     output logic MemWriteD,
     output logic ALUsrcD,
-    output logic ImmSrcD,
+    output logic [2:0] ImmSrcD,
     output logic [2:0] BranchD,
     output logic JumpD,
     output logic [2:0] ALUControlD
@@ -20,21 +20,21 @@ logic [1:0] instr_type;       // interconnect wire.
 main_decoder main_control (
     .funct3(funct3),
     .opcode(opcode),
-    .RegWrite(RegWriteD),
-    .ResultSrc(ResultSrcD),
-    .MemWrite(MemWriteD),
-    .ALUsrc(ALUsrcD),
-    .ImmSrc(ImmSrcD),
-    .branch(BranchD),
-    .jump(JumpD),
+    .RegWriteD(RegWriteD),
+    .ResultSrcD(ResultSrcD),
+    .MemWriteD(MemWriteD),
+    .ALUsrcD(ALUsrcD),
+    .ImmSrcD(ImmSrcD),
+    .BranchD(BranchD),
+    .JumpD(JumpD),
     .ALUOp(instr_type)
 );
 
 ALU_decoder alu_control (
     .ALUOp(instr_type),
     .funct3(funct3),
-    .funct7_bit(funct7[6]),
-    .ALUControl(ALUControlD)
-)
+    .funct7_bit(funct7[5]),
+    .ALUControlD(ALUControlD)
+);
     
 endmodule
