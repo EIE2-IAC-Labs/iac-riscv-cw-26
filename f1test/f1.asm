2b58a54c8c5cbbaeb4b4253fc4a9b1a7c939e672
mxwlc <maxwellcorey85@gmail.com>
2022-12-12 00:16:49 +0000

    changed assembly so it worked

--- f1test/f1.asm
+++ f1test/f1.asm
@@ -4,32 +4,37 @@ f1.out.reloc:     file format elf32-littleriscv
 
 Disassembly of section .text:
 
-bfc00000 <init> (File Offset: 0x1000):
-init():
-bfc00000:	0ff40413          	addi	s0,s0,255
-bfc00004:	03238393          	addi	t2,t2,50
-
-bfc00008 <main> (File Offset: 0x1008):
+bfc00000 <main> (File Offset: 0x1000):
 main():
-bfc00008:	00150513          	addi	a0,a0,1
-bfc0000c:	0080036f          	jal	t1,bfc00014 <routine> (File Offset: 0x1014)
-bfc00010:	fe851ce3          	bne	a0,s0,bfc00008 <main> (File Offset: 0x1008)
+bfc00000:	008000ef          	jal	ra,bfc00008 <init> (File Offset: 0x1008)
+bfc00004:	00c000ef          	jal	ra,bfc00010 <_loop> (File Offset: 0x1010)
 
-bfc00014 <routine> (File Offset: 0x1014):
+bfc00008 <init> (File Offset: 0x1008):
+init():
+bfc00008:	0ff40413          	addi	s0,s0,255
+bfc0000c:	03238393          	addi	t2,t2,50
+
+bfc00010 <_loop> (File Offset: 0x1010):
+_loop():
+bfc00010:	00150513          	addi	a0,a0,1
+bfc00014:	0080036f          	jal	t1,bfc0001c <routine> (File Offset: 0x101c)
+bfc00018:	fe851ce3          	bne	a0,s0,bfc00010 <_loop> (File Offset: 0x1010)
+
+bfc0001c <routine> (File Offset: 0x101c):
 routine():
-bfc00014:	00000013          	addi	zero,zero,0
-bfc00018:	00108093          	addi	ra,ra,1
-bfc0001c:	fe709ce3          	bne	ra,t2,bfc00014 <routine> (File Offset: 0x1014)
-bfc00020:	00000367          	jalr	t1,0(zero) # 0 <init-0xbfc00000> (File Offset: 0xffffffff40401000)
+bfc0001c:	00000013          	addi	zero,zero,0
+bfc00020:	00108093          	addi	ra,ra,1
+bfc00024:	fe709ce3          	bne	ra,t2,bfc0001c <routine> (File Offset: 0x101c)
+bfc00028:	00000367          	jalr	t1,0(zero) # 0 <main-0xbfc00000> (File Offset: 0xffffffff40401000)
 
 Disassembly of section .riscv.attributes:
 
-00000000 <.riscv.attributes> (File Offset: 0x1024):
+00000000 <.riscv.attributes> (File Offset: 0x102c):
    0:	1e41                	.2byte	0x1e41
    2:	0000                	.2byte	0x0
    4:	7200                	.2byte	0x7200
    6:	7369                	.2byte	0x7369
-   8:	01007663          	bgeu	zero,a6,14 <init-0xbfbfffec> (File Offset: 0x1038)
+   8:	01007663          	bgeu	zero,a6,14 <main-0xbfbfffec> (File Offset: 0x1040)
    c:	0014                	.2byte	0x14
    e:	0000                	.2byte	0x0
   10:	7205                	.2byte	0x7205
--- f1test/f1.s
+++ f1test/f1.s
@@ -1,13 +1,17 @@
 .text
 
+main:
+    JAL ra, init
+    JAL ra, _loop
+
 init:
     ADDI x8, x8, 0xFF
     ADDI x7, x7, 50
 
-main:
+_loop:
     ADDI a0, a0, 1
     JAL x6, routine
-    BNE a0, x8, main
+    BNE a0, x8, _loop
 
 routine:
     ADDI x0, x0, 0
