19b43fccd1855817e3655bf46f936636ad340d86
mxwlc <maxwellcorey85@gmail.com>
2022-12-15 11:00:13 +0000

    removed redundant code

--- memory/memory_tb.cpp
+++ memory/memory_tb.cpp
@@ -62,18 +62,6 @@ int main(int argc, char **argv, char **env) {
             top->lb = 1;
         }
 
-        
-        // // Send data to Vbuddy
-        // vbdCycle(i);
-        // vbdHex(1, top->a0 & 0xf);
-        // vbdHex(2, (top->a0 >> 4) & 0xf);
-        // vbdHex(3, (top->a0 >> 8) & 0xf);
-        // vbdHex(4, (top->a0 >> 12) & 0xf);
-        // // Although the register a0 is 32 bits, we will not be using it to store any numbers higher than 16 bits.
-
-        // Update input signals
-        // top->rst = i < 2;
-
     }
 
     // vbdClose();
