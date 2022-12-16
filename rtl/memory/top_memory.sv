141777a0459fa460fb182c2de833f9c09044d204
BlueCP <raekbote@gmail.com>
2022-12-16 20:34:19 +0000

    Update rtl folder

--- rtl/memory/top_memory.sv
+++ rtl/memory/top_memory.sv
@@ -3,7 +3,6 @@ module top_memory #(
     parameter WIDTH = 32
 ) (
     input logic clk,
-    // input logic memWrite_M,
     input logic sw,
     input logic sh,
     input logic sb,
@@ -16,16 +15,58 @@ module top_memory #(
     output logic [WIDTH-1:0] readData_M
 );
 
-logic [WIDTH-1:0] dout;
+// Internal signals
+logic loadIns, storeIns;
 
-ram ram(
+// Cache signals
+logic cache_hit, cache_evict;
+logic [WIDTH-1:0] cache_out, cache_store, cache_addr_out;
+
+// Other memory signals
+logic [WIDTH-1:0] ram_out, memory_out;
+
+always_comb begin
+    loadIns = lw || lh || lb;
+    storeIns = sw || sh || sb;
+    memory_out = cache_hit ? cache_out : ram_out;
+end
+
+// // // Cache // // //
+
+cache cache (
+    .clk(clk),
+    .loadIns(loadIns),
+    .storeIns(storeIns),
+    .addr_in(ALUResult_M),
+    .cache_store(cache_store),
+    .ram_out(ram_out),
+    .hit(cache_hit),
+    .evict(cache_evict),
+    .addr_out(cache_addr_out),
+    .dout(cache_out)
+);
+
+cache_store_input cache_store_input (
     .sw(sw),
     .sh(sh),
     .sb(sb),
-    .wd(writeData_M),
-    .a(ALUResult_M),
+    .din(writeData_M),
+    .addr(ALUResult_M),
+    .mem_out(memory_out),
+    .cache_store(cache_store)
+);
+
+// // // RAM // // //
+
+ram ram(
+    .sw(cache_evict),
+    .sh(0),
+    .sb(0),
+    .wd(cache_out),
+    .r_a(ALUResult_M),
+    .w_a(cache_addr_out),
     .clk(clk),
-    .rd(dout)
+    .rd(ram_out)
 );
 
 half_byte_word hbw(
@@ -33,7 +74,8 @@ half_byte_word hbw(
     .lh(lh),
     .lb(lb),
     .s(s),
-    .data(dout),
+    .data(memory_out),
+    .offset(ALUResult_M[1:0]),
     .dout(readData_M)
 );
 
